5e50da66dd20a504a1a8794dc6d7346dec923608
Daefron <thomas_evans@outlook.com>
2023-05-17 14:29:25 +1000

    add button functionality

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
+let board = ["O", "", "", "O", "", "", "O", "", ""];
+//function to get button inputs
+for (let i = 0; i < 9; ++i) {
+  document.getElementById("space" + i).addEventListener("click", () => {
+    if (board.at(i) == "") {
+      console.log(i + "X"); //replace X with turn
+      board.splice(i, 1, "X"); //replace X with turn
+      console.log(board);
+      return displayController();
+    }
+  });
+}
+//function to push board onto display
+function displayController() {
+  for (let i = 0; i < 9; ++i) {
+    document.getElementById(i).textContent = board[i];
+  }
+}
+displayController();
+//function to determine if/who wins
 //winning conditions
 // 0 1 2|3 4 5|6 7 8
 // X X X|     |       const row1 = [0,1,2];
@@ -8,15 +28,6 @@
 //     X|    X|    X  const column3 = [2,5,8];
 // X    |  X  |    X  const diag1 = [0,4,8];
 //     X|  X  |X      const diag2 = [2,4,6];
-let board = ["X", "", "O", "O", "X", "", "X", "O", "X"];
-
-function displayController() {
-  for (let i = 0; i < 9; ++i) {
-    document.getElementById(i).textContent = board[i];
-  }
-}
-displayController();
-
 function winner() {
   const winConditions = {
     row1: [0, 1, 2],
@@ -29,23 +40,23 @@ function winner() {
     diag2: [2, 4, 6],
   };
   Object.values(winConditions).forEach((condition) => {
-    console.log(condition);
     let win = 0;
     let lose = 0;
     condition.forEach((position) => {
-      console.log(position);
       if (board.at(position) == "X") {
-        console.log("X");
         return ++win;
       } else if (board.at(position) == "O") {
-        console.log("O");
         return ++lose;
       }
     });
     if (win == 3) {
-      return console.log("win");
+      console.log(winConditions[condition]);
+      console.log("win");
+      // return win();
     } else if (lose == 3) {
-      return console.log("lose");
+      console.log(condition);
+      console.log("lose");
+      // return lose();
     }
   });
 }
