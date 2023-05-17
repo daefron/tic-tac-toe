597f7ece5b8f91921dea6e1b30bb91dbe4eb3988
Daefron <thomas_evans@outlook.com>
2023-05-17 12:07:51 +1000

    add HTML frame and basic CSS

---
+++ code.js
@@ -0,0 +1,9 @@
+//winning conditions
+// X X X O O O O O O
+// O O O X X X O O O
+// O O O O O O X X X
+// X O O X O O X O O
+// O X O O X O O X O
+// O O X O O X O O X
+// X O O O X O O O X
+// O O X O X O X O O
